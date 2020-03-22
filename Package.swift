6a35474eea3484d47061e5af36541f593d84a58d
Federico Zanetello <zntfdr@gmail.com>
2020-03-22 12:59:15 +0700

    remove tests

--- Package.swift
+++ Package.swift
@@ -21,8 +21,5 @@ let package = Package(
         .target(
             name: "AStack",
             dependencies: []),
-        .testTarget(
-            name: "AStackTests",
-            dependencies: ["AStack"]),
     ]
 )
--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import AStackTests
-
-var tests = [XCTestCaseEntry]()
-tests += AStackTests.allTests()
-XCTMain(tests)
